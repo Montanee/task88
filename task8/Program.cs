7a53d4e7ea65ca632135825c437911f1f2f901b0
Administrator <Administrator@WIN-S09DU0P86JK>
2024-06-20 16:47:03 +0300

    Добавьте файлы проекта.

---
+++ task8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Введите два числа от 0 до 10:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            if ((number1 >= 0 && number1 <= 10) && (number2 >= 0 && number2 <= 10))
+            {
+                Console.WriteLine($"Результат умножения: {number1 * number2}");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Введенные числа недопустимы. Попробуйте снова.");
+            }
+        }
+    }
+}
